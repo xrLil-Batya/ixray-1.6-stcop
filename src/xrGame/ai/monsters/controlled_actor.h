42cf498fb1fa7efa07552addc147caa03f6b9bf5
OldSerpskiStalker <ictopiar@gmail.com>
2025-01-30 13:34:41 +0500

    Update controlled_actor.h

--- src/xrGame/ai/monsters/controlled_actor.h
+++ src/xrGame/ai/monsters/controlled_actor.h
@@ -29,7 +29,7 @@ public:
 			bool    is_installed		();
 
 			void	frame_update		();
-			bool	is_controlling		() {return m_actor != 0;}
+			bool	is_controlling		() {return m_actor != nullptr;}
 
 			void	dont_need_turn		(){m_need_turn = false;}
 
