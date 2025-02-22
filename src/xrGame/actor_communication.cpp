e967b29aa6e6f39334a77d96a1ad63edbaf9ee74
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-02-22 14:07:29 +0300

    Fix missing break dialog for `Clear Sky`

--- src/xrGame/actor_communication.cpp
+++ src/xrGame/actor_communication.cpp
@@ -108,6 +108,9 @@ void   CActor::UpdateAvailableDialogs	(CPhraseDialogManager* partner)
 	for(u32 i = 0; i<pInvOwnerPartner->CharacterInfo().ActorDialogs().size(); i++)
 		AddAvailableDialog(pInvOwnerPartner->CharacterInfo().ActorDialogs()[i], partner);
 
+	if (EngineExternal().ClearSkyMode())
+		AddAvailableDialog("actor_break_dialog", partner);
+
 	CPhraseDialogManager::UpdateAvailableDialogs(partner);
 }
 
